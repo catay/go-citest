d1d49faecd1f1ba6eef9c242706553e61d12e4e1
Steven Mertens <steven.mertens@catay.be>
2018-05-10 16:26:29 +0200

    Add some random test code.

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"os"
+)
+
+func main() {
+
+	fmt.Println("This is a test to make circleci work")
+
+	f, err := os.Open("/tmp/bla.txt")
+	if err != nil {
+		fmt.Println(errors.Wrap(err, "open failed"))
+	} else {
+		f.Close()
+	}
+
+}
